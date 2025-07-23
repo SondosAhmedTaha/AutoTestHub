ae50913ea03c4289f693f62a1d3c6ff47d5ee5cc
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2025-07-23 16:00:49 +0300

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    int age;
+    
+    std::cin >> name >> age;
+    std::cout << "Hello, " << name << "! You are " << age << " years old." << std::endl;
+    
+    return 0;
+}
